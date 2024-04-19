ed1eaed43a55bf3719c3f166eb7ee8cf65f53d70
Odusina Adeyemi Sodiq <odusina.adeyemi@gmail.com>
2024-04-19 19:03:20 +0100

    commit2

--- src/App.js
+++ src/App.js
@@ -1,40 +1,21 @@
-import './App.css';
-import '../src/categories.styles.scss';
-
-import Directory from './components/directory/Directory.component';
-
+import React from 'react'
+import Home from './routes/home/home.component';
+import {Routes, Route} from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-      id: 4,
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-      id: 5,
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    },
-  ];
-
   return (
-   <Directory categories={categories}/>
-  );
-};
+    <div>
+      <Routes>
+        
+        <Route path='/' element={<Navigation/>}>
+        <Route index element={<Home/>}/>
+        {/* <Route path='shop' element={<Shop/>}/> */}
+        </Route>
+        
+      </Routes>
+      
+    </div>
+  )
+}
 
-export default App;
+export default App
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import React, {Fragment} from "react";
+import { Outlet, Link } from "react-router-dom";
+import {ReactComponent as Crown} from '../../assets/crown.svg';
+import '../navigation/navigation.styles.scss'
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation" >
+        
+        <Link className="logo-container" to='/'>
+        <div><Crown className="logo"/></div>
+        </Link>
+        <div className="nav-links-container">
+        <Link className='nav-link' to='/shop'>
+        SHOP
+        </Link>
+        </div>
+      </div>
+
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
